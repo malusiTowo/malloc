c0d17307463dc4039108e453cf31d32bad185f27
Luis.Rosario <luis.rosario@epitech.eu>
2019-02-03 16:21:13 +0100

    [FIX] fixing coding style + show_alloc_mem

--- src/show_alloc_mem.c
+++ src/show_alloc_mem.c
@@ -14,6 +14,6 @@ void show_alloc_mem(void)
     for (; iter->next != NULL; iter = iter->next) {
         if (!iter->_isFree)
             printf("%p - %p : %ld bytes", iter + 1, \
-                   iter->next + 1, iter->next->_size - iter->_size);
+            iter->next + 1, iter->next->_size - iter->_size);
     }
 }
